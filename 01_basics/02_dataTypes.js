c3bbb24b413c8486b4fb01ef07949de5253a5d6e
Khondhoker Sazzadul Kadir <71392727+ahsazzad@users.noreply.github.com>
2024-01-05 06:23:16 +0000

    Data Types Discussion

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"Use Strict" //Turned older Version JS as newer version JS 
+
+// Data Types
+let name = "sazzad";
+let age = 18;
+let isLoggedIn = false;
+let state; // undefined;
+let value = null; // Null means no value;
+//Number
+//String
+//bigint
+//boolean
+//null -- Standalone value (Data type - Object)
+//undefined
+//symbol
+//object
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+console.log(typeof value); //Null=object 
